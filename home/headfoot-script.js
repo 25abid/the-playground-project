430a4ce9421f758993f0e6b57beb572fcba4823d
alturkm3 <abdulkader.alturkmani@mail.utoronto.ca>
2025-08-25 10:34:29 -0400

    Fixed page navigation and absolute paths

---
+++ home/headfoot-script.js
@@ -0,0 +1,9 @@
+function loadIncludes() {
+  fetch('/partials/header.html')
+    .then(res => res.text())
+    .then(data => document.getElementById('header').innerHTML = data);
+
+  fetch('/partials/footer.html')
+    .then(res => res.text())
+    .then(data => document.getElementById('footer').innerHTML = data);
+}
